86161f2bf7380bb016d016423cff706d8787358b
Nicolai Müller <nicolaimu1212@gmail.com>
2021-09-09 14:01:10 +0200

    Added File Upload and Metadata-Parser.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'public/uploads/'})
 require('dotenv').config()
 
 var app = express();
@@ -8,13 +10,22 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
 
+	let file = req.file;
+
+	res.json({
+		name: file.originalname,
+		type: file.mimetype,
+		size: file.size
+	})
+});
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log('Your app is listening on port ' + port)
 });
